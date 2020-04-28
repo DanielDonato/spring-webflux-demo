129d249e934e346fed0a706d6a31170fb1042990
DanielDonato <daniel_donato10@yahoo.com.br>
2020-04-28 14:16:48 -0300

    Criando um handler e um router

--- src/main/java/com/danieldonato/webflux/controller/PlaylistController.java
+++ src/main/java/com/danieldonato/webflux/controller/PlaylistController.java
@@ -14,7 +14,7 @@ import com.danieldonato.webflux.services.PlaylistService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController
+//@RestController
 @RequestMapping(value = "/playlist")
 public class PlaylistController {
 	
